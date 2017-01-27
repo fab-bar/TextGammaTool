a35109d11c87eeb4a1fce88a5e303d202435182e
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2017-01-27 09:28:39 +0100

    Make addition of file types to AnnotatedTextFileReaderFactory easier

--- src/main/java/io/AnnotatedTextFileReaderStaticFactory.java
+++ src/main/java/io/AnnotatedTextFileReaderStaticFactory.java
@@ -1,15 +1,30 @@
 package io;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AnnotatedTextFileReaderStaticFactory {
-	
-	public static AnnotatedTextFileReaderFactory createReaderFactory(String type) {
-		if (type.equals("CoraXML")) {
-			return new CoraXMLReaderFactory();
-		}
-		else if (type.equals("CoraXMLReN")) {
-			return new CoraXMLReNReaderFactory();
-		}
-		return null;
+
+	private static final Map<String, Class<? extends AnnotatedTextFileReaderFactory>> reader_types;
+	static {
+		Map<String, Class<? extends AnnotatedTextFileReaderFactory>> tmp_map;
+		tmp_map = new HashMap<>();
+		tmp_map.put("CoraXML", CoraXMLReaderFactory.class);
+		tmp_map.put("CoraXMLReN", CoraXMLReNReaderFactory.class);
+		// add further supported file types...
+		reader_types = Collections.unmodifiableMap(tmp_map);
+	}
+
+	public static String[] getFileReaderTypes() {
+		return  reader_types.keySet().toArray(new String[0]);
+	}
+
+	public static AnnotatedTextFileReaderFactory createReaderFactory(String type) throws InstantiationException, IllegalAccessException {
+		if (reader_types.containsKey(type))
+			return reader_types.get(type).newInstance();
+		else
+			return null;
 	}
 
 }
